a01a7c05c56ba414b799881e11b8b4851b6bf778
LOVEJOY <805197013@qq.com>
2016-11-12 16:41:54 +0800

    Add files via upload

---
+++ HW5/14301020/Office.java
@@ -0,0 +1,18 @@
+package test;
+
+public class Office {
+    private String officeId;
+
+    public String getOfficeId() {
+        return officeId;
+    }
+
+    public void setOfficeId(String officeId) {
+        this.officeId = officeId;
+    }
+
+    public String tostring() {
+        return "Office " + officeId;
+    }
+
+}
